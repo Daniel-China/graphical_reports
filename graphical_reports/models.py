2f71f20d41982c1cc1ff518beeff5018b0a16261
Daniel <yahui1105@qq.com>
2016-12-15 18:58:26 +0800

    周四酱油一天

--- graphical_reports/models.py
+++ graphical_reports/models.py
@@ -1,2 +1,12 @@
 #!/usr/bin/env python
 #encoding:utf8
+from  django.db import models
+
+
+
+class table_info(models.Model):
+    t_id = models.AutoField(primary_key=True)
+    t_theme = models.CharField(max_length=40)
+    t_title = models.CharField(max_length=100)
+    t_type = models.CharField(max_length=40)
+    t_iscofig = models.BooleanField()
