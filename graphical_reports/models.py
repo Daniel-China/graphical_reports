4767ae5ba90c236b8950c1f079e3db03420e9d02
Daniel <yahui1105@qq.com>
2017-02-27 20:57:24 +0800

    久违的更新

--- graphical_reports/models.py
+++ graphical_reports/models.py
@@ -76,3 +76,14 @@ class ConfigOption(models.Model):
 
     series_default = models.TextField()
     series_type = models.TextField()
+
+class DataSource():
+    type = models.CharField(max_length=40)
+    host = models.CharField(max_length=60)
+    port = models.IntegerField(max_length=20)
+    db_name = models.CharField(max_length=60)
+    user_name = models.CharField(max_length=60)
+    passwd = models.CharField(max_length=40)
+
+
+
